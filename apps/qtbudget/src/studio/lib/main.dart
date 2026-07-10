47117c9907f926f4e526c9ee6bf2b55101d6b4a2
Guo Zhang <zhangguo@quanttide.com>
2026-07-11 02:08:35 +0800

    cleanup: 删除qtbudget和qt-finance两个实验

--- apps/qtbudget/src/studio/lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'app.dart';
-import 'services/storage_backend_web.dart';
-import 'services/storage_service.dart';
-
-void main() {
-  StorageService.useBackend(LocalStorageBackend());
-  runApp(const QtBudgetApp());
-}
